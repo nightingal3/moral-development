6070830a97c375470f454a003609b2c878d859ba
Emmy Liu <emmy.liu.1998@gmail.com>
2021-07-07 15:27:14 -0400

    cluster childes for stories

--- src/data/cluster_childes.py
+++ src/data/cluster_childes.py
@@ -5,8 +5,7 @@ import pandas as pd
 from src.data.clustering_models import *
 from sentence_transformers import SentenceTransformer
 from sklearn.manifold import TSNE
-
-
+import pdb
 
 map_categories = {'care.virtue': 'harm', 'care.vice': 'harm', 'authority.virtue': 'authority',
                   'authority.vice': 'authority', 'loyalty.virtue': 'loyalty', 'loyalty.vice': 'loyalty',
@@ -33,12 +32,18 @@ def select_childes(data: pd.DataFrame, identity : str, age: int, category : str,
     new_data = list(data.loc[data.identity == identity].loc[data.year.astype(int) == age].loc[data.category.isin(categories)].context)
     return new_data
 
+def select_story_data(data: pd.DataFrame, lower_quantile_val: float, upper_quantile_val: float, category: str) -> list:
+    # categories = []
+    # for foundation, mapped_foundation in map_categories.items():
+    #     if mapped_foundation == category:
+    #         categories.append(foundation)
+    new_data = list(data.loc[(data.category == category) & ((data["flesch_kincaid"] >= lower_quantile_val) & (data["flesch_kincaid"] < upper_quantile_val))].sentence)
+    return new_data
+
 def get_embeddings(data: list) -> np.array:
     model = SentenceTransformer('bert-base-nli-mean-tokens')
     vectors = np.array([model.encode(x) for x in data])
     return vectors
-
-
 
 
 def assign_labels(labels, data, age_compressed_vectors,probs, has_probs = False):
@@ -100,23 +105,21 @@ def train_clustering_model(name, data):
 
     vectors = get_embeddings(data)
 
-
     best_k, labels, best_clustering_model = model.find_k(vectors)
-
-
-
-
 
     return best_k, labels,model, vectors
 
 
 
-def category_cluster_by_age(childes_data: pd.DataFrame, category, model_name):
+def category_cluster_by_age(childes_data: pd.DataFrame, category, model_name, text_type: str = "childes"):
     ages = range(1, 7)
     age_datats = []
     vectors_of_ages = []
     for age in ages:
-        data = select_childes(childes_data, 'child', age, category)
+        if text_type == "childes":
+            data = select_childes(childes_data, 'child', age, category)
+        else:
+            data = select_story_data(childes_data, category)
 
         best_k,labels, model, vectors = train_clustering_model(model_name, data)
         age_datats.append((best_k,labels, model, vectors,data))
@@ -180,11 +183,53 @@ def category_cluster(childes_data: pd.DataFrame, category, model_name):
                     open(os.path.join('data', model_name, category, f'{age}_all.pkl'), 'wb'))
 
 
+def category_cluster_stories(stories_data: pd.DataFrame, category, model_name) -> None:
+    all_utterances =  []
+    age_utterances = []
+    age_vectors = []
+    quantiles = {i * 0.1: stories_data["flesch_kincaid"].quantile(i * 0.1) for i in range(1, 11)}
+
+    for i, value in enumerate(quantiles):
+        if i == len(quantiles) - 1:
+            break
+        data = select_story_data(stories_data, quantiles[value], quantiles[(i + 2) * 0.1], category)
+        vectors = get_embeddings(data)
+        age_utterances.append(data)
+        age_vectors.append(vectors)
+        all_utterances += data
+
+    best_k, labels, model, vectors = train_clustering_model(model_name, all_utterances)
+    compressed_vectors = prepare_visualization(age_vectors)
+
+    for i, age in enumerate(quantiles):
+        if i == len(quantiles) - 1:
+            break
+        data = age_utterances[i]
+        v = age_vectors[i]
+        age_compressed_vectors = compressed_vectors[i]
+
+        age_labels = model.predict(v)
+
+
+        if model_name == 'GMM':
+            probs = model.get_probs(v)
+
+            labels_utterances = assign_labels(age_labels, data, age_compressed_vectors, probs, has_probs=True)
+
+        else:
+            probs = None
+            labels_utterances = assign_labels(age_labels, data, age_compressed_vectors, probs, has_probs=False)
+
+        pickle.dump((labels_utterances, probs, best_k, model),
+                    open(os.path.join('data/full/', model_name, category, f'{age}_all.pkl'), 'wb'))
+
+
+
 if __name__ == '__main__':
-    df = pickle.load(open('data/pickled-data/moral_df_context_2.p', 'rb'))
-
-
-    #
+    df = pickle.load(open('data/moral_df_context_2.p', 'rb'))
+    df_stories = pickle.load(open("data/story-sentences-v2.p", "rb"))
+    
+        #
     # for m in models:
     #     for c in categories:
     #         category_cluster_by_age(df,c, m)
@@ -192,7 +237,7 @@ if __name__ == '__main__':
 
     for m in models:
         for c in categories:
-            category_cluster(df, c, m)
+            category_cluster_stories(df_stories, c, m)
 
 
 
